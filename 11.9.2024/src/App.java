85caa95852ee25305ab4d339062d235f1c960d61
Uusitalo03 <lauri.uusitalo03@gmail.com>
2024-09-11 09:23:38 +0300

    Muutos 1
    Muutos 1

---
+++ 11.9.2024/src/App.java
@@ -0,0 +1,31 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+   // Kännykkä. Jos kännykkä on päällä, kirjoitetaan "on"
+   // Jos ei, kirjoitetaan "ei"
+
+   //JOS paalla= true NIIN
+   // Tulostetaan: "on päällä"
+   //MUUTEN
+   // Tulostetaan "Ei ola päällä"
+   // LOPPU-JOS
+
+   boolean paalla = false;
+   int paalla2 = 0; // 0 = ei ole päällä, 1 = on päällä, 2 = virhetila
+
+if (paalla == true)
+{
+// Jos ehto on tosi, tämän lohkon/blockin koodit toteutuvat
+System.out.println("Kännykkä on päällä");
+
+}
+else
+{
+  System.out.println("Kännykkä ei ole päällä");  
+  }
+   
+
+    }
+
+{}}
+
